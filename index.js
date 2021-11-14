a1067424bc64099f6de9237c07a8ff0bbcfc4abc
fawzirjili <rjilifawzi@gmail.com>
2021-11-15 00:33:17 +0100

    rendering html

--- index.js
+++ index.js
@@ -3,21 +3,18 @@ const {google} = require("googleapis");
 
 const app=express();
 
-app.get("/", async (req,res)=>{
-    const auth = new google.auth.GoogleAuth(
-        {
-            keyFile :"credentials.json",
-            scopes : "https://googleapis.com/auth/spreadsheets",
-        }
-    )
-    // create client instance for auth
-    const client = await auth.getClient();
+app.use(express.static("public"))
+app.use('/css',express.static(__dirname + 'public/css'));
+app.use('/img',express.static(__dirname + 'public/img'));
+app.use('/js',express.static(__dirname + 'public/js'));
+app.use('/lib',express.static(__dirname + 'public/lib'));
 
-    //instance of Googlesheets API
-    const googlesheets = google.sheets({version:"v4",auth:client});
-    
-    res.send("hello world");
-});
+app.set("view engine", "ejs");
+app.set("views","./views");
+
+app.get("/", async (req,res)=>{
+    res.render("index");
+})
 
 app.listen(1234,(req,res)=>console.log("app running on 1234"));
 
