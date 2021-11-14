ed1c9bef15173d7215478e155fae842c280b6b57
fawzirjili <rjilifawzi@gmail.com>
2021-11-14 17:39:50 +0100

    auth

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const {google} = require("googleapis");
 
 const app=express();
 
-app.get("/",(req,res)=>{
+app.get("/", async (req,res)=>{
+    const auth = new google.auth.GoogleAuth(
+        {
+            keyFile :"credentials.json",
+            scopes : "https://googleapis.com/auth/spreadsheets",
+        }
+    )
+    // create client instance for auth
+    const client = await auth.getClient();
+
+    //instance of Googlesheets API
+    const googlesheets = google.sheets({version:"v4",auth:client});
+    
     res.send("hello world");
 });
 
