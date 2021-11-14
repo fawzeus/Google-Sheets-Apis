b45f82c80dc3badb75e89ceb2ddcb5384e612ccc
fawzirjili <rjilifawzi@gmail.com>
2021-11-15 00:49:26 +0100

    fix port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {google} = require("googleapis");
 
 const app=express();
-
+const port = 3000 ;
 app.use(express.static("public"))
 app.use('/css',express.static(__dirname + 'public/css'));
 app.use('/img',express.static(__dirname + 'public/img'));
@@ -16,5 +16,6 @@ app.get("/", async (req,res)=>{
     res.render("index");
 })
 
-app.listen(1234,(req,res)=>console.log("app running on 1234"));
+//app.listen(1234,(req,res)=>console.log("app running on 1234"));
+app.listen(process.env.PORT || port, ()=> console.log(`listning on port ${port}`))
 
